aee26274daca2c34ee2f7a1556c1f6f066f5687c
j2FunOnly <j2funonly@ya.ru>
2018-10-23 10:44:42 +0300

    Fixed paths issues

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ gulp.task('copy-js', function () {
   return gulp
     .src([
       'node_modules/bootstrap/dist/js/bootstrap.min.js',
-      'node_modules/popper.js/dist/popper.min.js',
+      'node_modules/popper.js/dist/umd/popper.min.js',
       'node_modules/jquery/dist/jquery.min.js'
     ])
     .pipe(gulp.dest('src/js'))
@@ -31,7 +31,7 @@ gulp.task('copy-fonts', function () {
 gulp.task('copy-css', function () {
   return gulp
     .src([
-      'node_modules/bootstrap/css/bootstrap.min.css',
+      'node_modules/bootstrap/dist/css/bootstrap.min.css',
       'node_modules/font-awesome/css/font-awesome.min.css'
     ])
     .pipe(gulp.dest('src/css'))
